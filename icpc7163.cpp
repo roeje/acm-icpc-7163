d85b6e7a4546f3bafc4cabba7268418a0200764d
Jesse Roe <jessedroe@gmail.com>
2015-11-30 07:01:00 +0800

    Update working. Modifying for summission.

--- icpc7163.cpp
+++ icpc7163.cpp
@@ -6,32 +6,47 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
-#include <c++/tuple>
 
 using namespace std;
 
 int solver (int& x, int& n, int& y, int& m);
+void read_values(int &x, int &n, int &y, int &m, string &io);
 int gcd(int a, int b);
+vector <int> vec;
 
-void readData(istream& input) {
-    int cases;
-//    cout << "Case Value" << to_string(cases) << endl;
-    input >> cases;
-    int x, n, y, m;
-    int count = 1;
-    while (cases > 0){
-        input >> x >> n >> y >> m;
-//        cout << "x = " << to_string(x) << "n = " << to_string(n) << "y = " << to_string(y) << "m = " << to_string(m) << endl;
-        cout << "Case " << to_string(count) << ": " << to_string(solver(x, n, y, m)) << endl;
-        cases--;
-        count++;
+//void readData(istream& input) {
+//    int cases;
+//    input >> cases;
+//    int x, n, y, m;
+//    int count = 1;
+//
+//    while (cases > 0){
+//        string temp;
+//        getline(cin, temp);
+//        read_values(x, n, y, m, temp);
+////        cout << to_string(vec.at(0)) << to_string(vec.at(1)) << to_string(vec.at(2)) << to_string(vec.at(3)) << endl;
+//        cout << "Case " << to_string(count) << ": " << to_string(solver(vec.at(0), vec.at(1), vec.at(2), vec.at(3))) << endl;
+//        vec.clear();
+//        cases--;
+//        count++;
+//    }
+//}
+
+void read_values(int &x, int &n, int &y, int &m, string &io){
+    stringstream ss(io);
+    string temp = "";
+    while(ss.good()){
+        ss >> temp;
+        if(temp != "") {
+            vec.push_back(stoi(temp));
+        }
     }
 }
 
 int solver (int& x, int& n, int& y, int& m) {
     pair<int, int> sum (x, 0);
     for (int i= 0; i < m; i++){
-        if(i == (n - 1)) {
+        if(i == n) {
             if(sum.first >= sum.second) {
                 sum.first += y;
             }
@@ -48,6 +63,7 @@ int solver (int& x, int& n, int& y, int& m) {
             }
         }
     }
+//    cout << "first: " << sum.first << " second: " << sum.second << endl;
     return gcd(sum.first, sum.second);
 }
 
@@ -56,15 +72,40 @@ int gcd(int a, int b) {
 }
 
 int main() {
-    string ex_data = "";
-    cout << "enter the data: ";
+//    string ex_data = "";
+//    cout << "enter the data: ";//
+//    cin.clear();
+//    getline(cin, ex_data);
+//    istringstream ss(ex_data);
+//    readData(ss);
 
-    cin.clear();
-    getline(cin, ex_data);
 
-
-    istringstream ss(ex_data);
-    readData(ss);
-//    istream &data = cin;
+    istream &data = cin;
 //    readData(data);
+
+    int cases;
+    input >> cases;
+    int x, n, y, m;
+    int count = 1;
+
+    while (cases > 0){
+        string temp;
+        getline(cin, temp);
+        read_values(x, n, y, m, temp);
+//        cout << to_string(vec.at(0)) << to_string(vec.at(1)) << to_string(vec.at(2)) << to_string(vec.at(3)) << endl;
+        cout << "Case " << to_string(count) << ": " << to_string(solver(vec.at(0), vec.at(1), vec.at(2), vec.at(3))) << endl;
+        vec.clear();
+        cases--;
+        count++;
+    }
+
+//    string temp = "";
+//    istringstream is;
+//    while(getline(cin, temp)) {
+//
+//    }
 }
+
+//2
+//4 3 6 5
+//5 1 15 2
